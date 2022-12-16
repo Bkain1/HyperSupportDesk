fcf971da1375fe4ea7adeb0f4ded30517a1d07b9
Matt K Hering <mhering3@student.cvtc.edu>
2022-12-15 21:19:35 -0600

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+-- Create user table and insert a test user
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
@@ -8,6 +9,7 @@ CREATE TABLE users (
 
 INSERT INTO users (name, email, password, usertype) VALUES ('Test', 'test@test.com', 'password', 0);
 
+-- Create ticket table and insert a test ticket
 CREATE TABLE tickets (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
@@ -19,6 +21,8 @@ CREATE TABLE tickets (
 
 INSERT INTO tickets (title, description, author, priority, status) VALUES ('Test Title', 'Test desc', 'Test author', 'Low', 'Waiting');
 
+-- Update a users usertype
 UPDATE users SET usertype = 1 WHERE id = 1;
 
+-- Delete a ticket from the db
 DELETE FROM tickets WHERE id = 1;
